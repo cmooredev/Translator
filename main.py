746632edd1ef4edfa035e2cc73e668ffc91b686d
cmooredev <46297716+cmooredev@users.noreply.github.com>
2022-06-22 21:15:05 -0700

    initialize bot

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 import discord
+import config
 from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = commands.Bot(command_prefix = '.', intents=intents)
+
+@client.event
+async def on_ready():
+    print('Bot is online')
+
+client.run(config.KEY)
