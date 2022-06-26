409c6211a436d051156f3380a8711a7bdd22ddfb
cmooredev <46297716+cmooredev@users.noreply.github.com>
2022-06-25 20:43:29 -0700

    fix embed

--- cogs/translate.py
+++ cogs/translate.py
@@ -38,10 +38,8 @@ class Translate(commands.Cog):
             #embedded message with op name and avatar
             #--# TODO: Custom color based on Language? Channel?
             embed=discord.Embed(title=message.author.display_name, description=result)
+            embed.set_thumbnail(url=message.author.avatar_url)
             await message.channel.send(embed=embed)
-            #send embedded message
-
-            await message.channel.send(result)
 
     @commands.command()
     async def ping(self, ctx):
