a1d778d43d7c3de50afaf8f92d6ca6ea190adc18
cmooredev <46297716+cmooredev@users.noreply.github.com>
2022-06-25 18:09:01 -0700

    m

--- cogs/translate.py
+++ cogs/translate.py
@@ -18,7 +18,7 @@ class Translate(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        await message.channel.send(message.author.mention)
+        
 
     @commands.command()
     async def ping(self, ctx):
