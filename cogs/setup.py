386b73e31b5c90a9387b7400142080d27f71a8d9
cmooredev <46297716+cmooredev@users.noreply.github.com>
2022-06-28 12:59:32 -0700

    reimplement select menus

--- cogs/setup.py
+++ cogs/setup.py
@@ -1,7 +1,25 @@
 import discord
 from discord.ext import commands
-from discord.ui import Select
 
+class Select(discord.ui.Select):
+    def __init__(self):
+        options=[
+            discord.SelectOption(label="English", emoji="🇺🇸", description="English"),
+            discord.SelectOption(label="Spanish", emoji="🇪🇸", description="Spanish"),
+            discord.SelectOption(label="French", emoji="🇫🇷", description="French")
+        ]
+        super().__init__(placeholder="Languages",
+            max_values=1, min_values=1, options=options)
+
+class SelectView(discord.ui.View):
+    def __init__(self, *, timeout = 100):
+        super().__init__(timeout=timeout)
+        self.add_item(Select())
+
+@commands.command()
+async def config(self, ctx):
+    #send select menu to user
+    await ctx.send("Select what language you would like to translate text to:", view=Select())
 
 class Setup(commands.Cog):
 
@@ -12,20 +30,7 @@ class Setup(commands.Cog):
     async def on_ready(self):
         #need to implement setup that lets users configure target lang
         #select = Select()
-
         print('SetupCog loaded')
-
-    @commands.command()
-    async def config(self, ctx):
-        select = Select(options=[
-            discord.SelectOption(label="English", emoji="🇺🇸", description="English"),
-            discord.SelectOption(label="Spanish", emoji="🇪🇸", description="Spanish"),
-            discord.SelectOption(label="French", emoji="🇫🇷", description="French"),
-        ])
-        view = View()
-        view.add_item(select)
-
-        await ctx.send("Select what language you would like to translate text to:", view=view)
 
 async def setup(client):
     await client.add_cog(Setup(client))
