ad08dfb98ea620f62749ce2f8d07fbad7bab2cd9
cmooredev <46297716+cmooredev@users.noreply.github.com>
2022-07-05 19:00:16 -0700

    test

--- cogs/translate.py
+++ cogs/translate.py
@@ -63,7 +63,7 @@ class Translate(commands.Cog):
     @commands.command()
     async def ping(self, ctx):
         lang = col.find_one()
-        await ctx.send(f'Current target language: {lang['target_lang']}')
+        await ctx.send(f'Current target language: {lang[0]}')
 
 async def setup(client):
     await client.add_cog(Translate(client))
