ec0d3da8883d40671b52fafb42afa691eb281850
cmooredev <46297716+cmooredev@users.noreply.github.com>
2022-07-05 17:28:42 -0700

    test

--- cogs/setup.py
+++ cogs/setup.py
@@ -30,7 +30,7 @@ class SelectLanguage(discord.ui.Select):
 
 
         server_key = {"server_id" : server_id}
-        result = insert_one(server_key)
+        result = replace_one(server_key, specs, True)
 
         await interaction.response.send_message(content=f"Your choice is {self.values[0]}", ephemeral=True)
 
