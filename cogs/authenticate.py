2cebcdc31749133f10767443253255dec32b0752
cmooredev <46297716+cmooredev@users.noreply.github.com>
2022-07-08 09:52:39 -0700

    add cog

--- cogs/authenticate.py
+++ cogs/authenticate.py
@@ -1,2 +1,17 @@
+import discord
+from discord.ext import commands
+
+class Authenticate(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Authenticate loaded')
+
 def auth_apikey():
-    print('authenticating....') 
+    print('authenticating....')
+
+async def setup(client):
+    await client.add_cog(Authenticate(client))
