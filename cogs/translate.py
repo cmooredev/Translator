a2ad9c76c3555364ab48b00d9bb8b1280939ac7b
cmooredev <46297716+cmooredev@users.noreply.github.com>
2022-07-12 12:24:03 -0700

    test translate

--- cogs/translate.py
+++ cogs/translate.py
@@ -44,7 +44,7 @@ class Translate(commands.Cog):
     async def on_ready(self):
         gtranslate_client = translate.Client(credentials=CREDENTIALS)
         print(gtranslate_client)
-        test_result = gtranslate_client.translate('hey i just want to test you', target_lang='en')
+        test_result = gtranslate_client.translate('donde esta mis amigos?', target_lang='en')
         print(f'TEST-{test_result}')
         print('TranslatorCog loaded')
 
