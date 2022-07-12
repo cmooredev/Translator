4c2784151b2936ddcfb028c93df37032b830f34b
cmooredev <46297716+cmooredev@users.noreply.github.com>
2022-07-11 17:32:39 -0700

    check date

--- cogs/authenticate.py
+++ cogs/authenticate.py
@@ -3,14 +3,15 @@ from discord.ext import commands
 from dotenv import load_dotenv
 import os
 import pymongo
+from datetime import datetime
 
 load_dotenv()
 DEEPL_AUTH = os.getenv('DEEPL_AUTH')
 MONGO_URI = os.getenv('MONGO_URI')
 mongodb_client = pymongo.MongoClient(MONGO_URI)
 
-db = mongodb_client["translatordb"]
-col = db["api_keys"]
+db = mongodb_client['translatordb']
+col = db['api_keys']
 
 
 class Authenticate(commands.Cog):
@@ -26,6 +27,12 @@ def auth_apikey(server_id):
     print(f'authenticating....')
     server_key = {'server_id': server_id}
     server_access = col.find_one(server_key)
+    reg_date = server_access['registration_date']
+    print(reg_date)
+    days_to_expired = datetime.now() - reg_date
+    if days_to_expired > 0:
+        print('not expired')
+
     if server_access is None:
         print('access denied')
         return False
