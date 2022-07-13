d9ed5e5112da69bc79af896477b94045cef157d3
cmooredev <46297716+cmooredev@users.noreply.github.com>
2022-07-12 17:10:11 -0700

    update

--- cogs/setup.py
+++ cogs/setup.py
@@ -33,7 +33,7 @@ class SelectLanguage(discord.ui.Select):
         super().__init__(placeholder="Languages",
             max_values=1, min_values=1, options=options)
     async def callback(self, interaction: discord.Interaction):
-        self.disabled = True
+        interaction.disabled = True
         #get server id to store specific target languages for multiple servers
         server_id = interaction.guild.id
         specs = {
