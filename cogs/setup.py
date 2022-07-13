26823b02b0f807bde3151ea54aac46524aec7a55
cmooredev <46297716+cmooredev@users.noreply.github.com>
2022-07-12 18:56:03 -0700

    test deleting view

--- cogs/setup.py
+++ cogs/setup.py
@@ -74,9 +74,10 @@ class Setup(commands.Cog):
         #send select menu to user
         select_view = SelectView()
         msg = await ctx.send("Select what language you would like to translate text to:", view=select_view)
+        await msg.delete()
         while interaction_finished is False:
             sleep(10)
-        await msg.delete()
+
 
 async def setup(client):
     await client.add_cog(Setup(client))
