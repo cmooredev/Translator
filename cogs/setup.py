cf4b12213b757a68afccd91c9831e9a7ba72373b
cmooredev <46297716+cmooredev@users.noreply.github.com>
2022-07-12 17:21:38 -0700

    update

--- cogs/setup.py
+++ cogs/setup.py
@@ -46,7 +46,7 @@ class SelectLanguage(discord.ui.Select):
 
         await interaction.response.send_message(content=f"Your choice is {self.values[0]}", ephemeral=True)
 
-        await interaction.delete_original_message()
+        await self.delete_original_message()
 
 class SelectView(discord.ui.View):
     def __init__(self, *, timeout = 60):
