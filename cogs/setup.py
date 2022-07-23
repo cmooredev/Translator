d3f07163a15738644b4e3a5dcc264fb36971f834
cmooredev <46297716+cmooredev@users.noreply.github.com>
2022-07-23 16:03:43 -0700

    testing adding users to subdoc

--- cogs/setup.py
+++ cogs/setup.py
@@ -75,7 +75,9 @@ class Setup(commands.Cog):
         server_key = {"server_id" : server_id}
         user_id = ctx.author.id
         user_choice =  [
-                {str(user_id) : "lang"}
+                {"id" : user_id,
+                 "choice": "lang"    
+                }
         ]
         result = col.update_one(server_key, {'$set': {"user_langs": user_choice}}, True)
         #send select menu to user
