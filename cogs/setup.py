04e30d58557e45db85051fef5145bb06cd45380b
cmooredev <46297716+cmooredev@users.noreply.github.com>
2022-07-23 16:23:37 -0700

    testing adding users to subdoc

--- cogs/setup.py
+++ cogs/setup.py
@@ -75,11 +75,11 @@ class Setup(commands.Cog):
         server_key = {"server_id" : server_id}
         user_id = ctx.author.id
         user_choice =  [
-                {"id" : user_id,
+                {
                  "choice": "lang"
                 }
         ]
-        result = col.update_one(server_key, {'$set': {"user_langs": [user_choice]}}, True)
+        result = col.update_one(server_key, {'$set': {user_id: user_choice}}, True)
         #send select menu to user
         select_view = SelectView()
         msg = await ctx.send("Select what language you would like to translate text to: \nThis message will delete in 10 seconds.", view=select_view, delete_after=10)
