6d1476ebbf57bcdad188bf2b987ea9274a4f8cfb
cmooredev <46297716+cmooredev@users.noreply.github.com>
2022-07-23 16:07:55 -0700

    testing adding users to subdoc

--- cogs/setup.py
+++ cogs/setup.py
@@ -11,6 +11,7 @@ MONGO_URI = os.getenv('MONGO_URI')
 mongodb_client = pymongo.MongoClient(MONGO_URI)
 db = mongodb_client["translatordb"]
 col = db["api_keys"]
+sub_doc = col["test"]
 
 #select menu for choosing a target language
 class SelectLanguage(discord.ui.Select):
@@ -76,10 +77,10 @@ class Setup(commands.Cog):
         user_id = ctx.author.id
         user_choice =  [
                 {"id" : user_id,
-                 "choice": "lang"    
+                 "choice": "lang"
                 }
         ]
-        result = col.update_one(server_key, {'$set': {"user_langs": user_choice}}, True)
+        result = sub_doc.update_one(server_key, {'$set': {"user_langs": user_choice}}, True)
         #send select menu to user
         select_view = SelectView()
         msg = await ctx.send("Select what language you would like to translate text to: \nThis message will delete in 10 seconds.", view=select_view, delete_after=10)
