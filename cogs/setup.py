5f3f203d24c6bcfab783727ce7480ffdaa966e24
cmooredev <46297716+cmooredev@users.noreply.github.com>
2022-07-23 21:41:45 -0700

    run test

--- cogs/setup.py
+++ cogs/setup.py
@@ -52,7 +52,7 @@ class SelectLanguage(discord.ui.Select):
         result = col.update_one(server_key, {'$set':specs}, True)
         result = col.update_one(server_key, {'$set': {f"user_langs.{user_id}": user_choice}}, True)
 
-        test_rest = col.find(server_key)
+        test_rest = col.find_one(server_key)
         test_rest = test_rest['user_langs']
         print(f'this is the test rest {test_rest}')
 
