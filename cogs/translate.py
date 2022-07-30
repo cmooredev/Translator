bcd1a63ed332e1c817fdb5be02559538ca20555f
cmooredev <46297716+cmooredev@users.noreply.github.com>
2022-07-30 15:18:27 -0700

    update

--- cogs/translate.py
+++ cogs/translate.py
@@ -83,7 +83,7 @@ class Translate(commands.Cog):
         server_credits = server_sub['credits']
         ### this is where u made change to test
         server_lang = server_sub['user_langs']
-        print(f'SERVER LANG ----------> server_lang <-----------')
+        print(f'SERVER LANG ----------> {server_lang} <-----------')
         ###
 
         if 'Translate' in str(message.author.roles):
