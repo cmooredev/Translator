541aefbeef0fa9a3bc0eb53d2da79b11794e34fd
cmooredev <46297716+cmooredev@users.noreply.github.com>
2022-07-30 17:02:07 -0700

    update

--- cogs/setup.py
+++ cogs/setup.py
@@ -50,7 +50,7 @@ class SelectLanguage(discord.ui.Select):
         server_key = {"server_id" : server_id}
         #update server info
         result = col.update_one(server_key, {'$set':specs}, True)
-        result = col.update_one(server_key, {'$set': {f"user_langs.{user_id}": user_choice}}, True)
+        #result = col.update_one(server_key, {'$set': {f"user_langs.{user_id}": user_choice}}, True)
 
 
         ## retrieve a user's lang
